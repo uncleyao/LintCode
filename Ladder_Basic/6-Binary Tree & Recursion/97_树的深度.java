8084baad1bb25524929e20131b822012b245f252
Yuan Yao <pengpeng9045@gmail.com>
2020-10-31 03:49:23 -0400

    中文

--- Ladder_Basic/6-Binary Tree & Recursion/97_树的深度.java
+++ Ladder_Basic/6-Binary Tree & Recursion/97_树的深度.java
@@ -2,26 +2,7 @@
 Given a binary tree, find its maximum depth.
 
 The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
-
-Example
-Example 1:
-
-Input: tree = {}
-Output: 0
-Explanation: The height of empty tree is 0.
-Example 2:
-
-Input: tree = {1,2,3,#,#,4,5}
-Output: 3	
-Explanation: Like this:
-   1
-  / \                
- 2   3                
-    / \                
-   4   5
-it will be serialized {1,2,3,#,#,4,5}
-Notice
-The answer will not exceed 5000
+获得树的深度
 
 */
 
@@ -58,5 +39,18 @@ public class Solution {
         height = Math.max(depth,height);
         traverse(root.left, depth+1);
         traverse(root.right,depth+1);
+    }
+}
+
+public class Solution2 {
+    
+   
+    public int TreeDepth(TreeNode root) {
+        if (root==null){
+            return 0;
+        }
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+        return Math.max(left,right)+1;
     }
 }
---
+++ Ladder_Basic/6-Binary Tree & Recursion/从上往下打印二叉树.java
@@ -0,0 +1,49 @@
+/*
+从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+
+*/
+/*
+思路：
+利用队列：先加root
+每次拿出queue的第一个node然后把left, right 加到queue的最后
+再保存这个node的val到result里
+
+*/
+
+
+import java.util.ArrayList;
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        ArrayList<TreeNode> queue = new ArrayList<>();
+        if (root==null){
+            return result;
+        }
+        queue.add(root);
+        while (queue.size()!=0){
+            TreeNode temp = queue.remove(0);
+            if (temp.left!=null){
+                queue.add(temp.left);
+            }
+            if (temp.right!=null){
+                queue.add(temp.right);
+            }
+            result.add(temp.val);
+        }
+        return result;
+    }
+}  }
+}
